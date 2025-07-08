96c6eda2c70f28777e3bedf8acbd2d281da89a29
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-07-08 18:58:04 +0300

    added an option to remove all the items at a certain label

--- commands/remove.py
+++ commands/remove.py
@@ -1,11 +1,14 @@
+from typing import Optional
 import click
 
 from helpers import clipboad_context
 
 @click.command(help='remove a copied text')
-@click.argument('label')
-def remove(label: str) -> None:
+@click.option('-l', 'label', help='remove copied text with its label')
+@click.option('-rp', '--remove-priority', help='remove all copied text with a certain level of priority', default=None)
+def remove(label: str, remove_priority: Optional[str]) -> None:
     clipboard_content: dict = clipboad_context.read_json()
+    remove_from_clipboard_content: dict = {}
     
     for copied_text in clipboard_content:
         data: dict = clipboard_content.get(copied_text)
@@ -14,5 +17,15 @@ def remove(label: str) -> None:
             clipboard_content.pop(copied_text)
             clipboad_context.write_json(data_to_write=clipboard_content)
             return
-    
-    click.echo("The label doesn't exist")
+        if not remove_priority is None:
+            remove_priority_as_num: int = int(remove_priority)
+            if data.get('priority') != remove_priority_as_num:
+                remove_from_clipboard_content.update({
+                    copied_text:{
+                        'label': data.get('label'), 
+                        'priority': data.get('priority')
+                    }
+                })
+                clipboad_context.write_json(data_to_write=remove_from_clipboard_content)
+            else:
+                click.echo(f'removed {copied_text}')
