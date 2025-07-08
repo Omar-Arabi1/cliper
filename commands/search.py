bf9e178d95388e157a2fa723becb92431c78ad8d
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-07-09 00:15:06 +0300

    created the fuzzy search in search.py

--- commands/search.py
+++ commands/search.py
@@ -1,5 +1,30 @@
 import click
+from fuzzywuzzy import process
+
+from helpers import clipboad_context
 
 @click.command(help='search through the labels you have')
-def search() -> None:
-    pass
+@click.argument('query')
+def search(query: str) -> None:
+    clipboard_contents: dict = clipboad_context.read_json()
+    labels: list[str] = []
+    
+    for copied_text in clipboard_contents:
+        data: dict = clipboard_contents.get(copied_text)
+        labels.append(data.get('label'))
+    
+    matches: list[tuple] = process.extract(query, labels)
+    best_matches: list[str] = []
+    
+    for index in range(len(matches)):
+        match_data: tuple = matches[index]
+        accuracy: int = match_data[1]
+        if accuracy >= 80:
+            match: str = match_data[0]
+            best_matches.append(match)
+    
+    for index, best_match in enumerate(best_matches):
+        num_of_match: int = index + 1
+        print(f"{num_of_match}) {best_match}")
+        
+        
