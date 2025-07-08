0a5fc00fc16c19fa17aa3a109daed451a7293540
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-07-09 00:00:51 +0300

    added the list command

--- commands/list.py
+++ commands/list.py
@@ -1,5 +1,13 @@
 import click
 
+from helpers import clipboad_context
+
 @click.command(help='list through all the saved copied texts you have')
 def list() -> None:
-    pass
+    cliboard_contents: dict = clipboad_context.read_json()
+    
+    for copied_text in cliboard_contents:
+        data: dict = cliboard_contents.get(copied_text)
+        label: str = data.get('label')
+        prioirty: int = data.get('priority')
+        print(f"{label}) '{copied_text}' -priority {prioirty}")
