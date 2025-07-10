5ef381f29e724bffe39cf809e0c4e726005ffed6
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-07-11 02:25:48 +0300

    fixed the versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cliper',
-    version='0.1.0',
+    version='1.0.0',
     description='a clipboard history tool',
     author='Omar Arabi',
     packages=find_packages(),
