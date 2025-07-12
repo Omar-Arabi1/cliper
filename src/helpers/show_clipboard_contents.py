800deb5458bb7bf8e6828bf6d6ba5225788de560
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-07-12 19:28:10 +0300

    added some code documentation"

--- src/helpers/show_clipboard_contents.py
+++ src/helpers/show_clipboard_contents.py
@@ -1,6 +1,21 @@
 from tabulate import tabulate
 
 def show_clipboard_contents(clipboard_contents: dict) -> None:
+    """
+    shows the clipboard's contents (creation_date, label, contents, priority)
+    in a tabular format
+    
+    PARAMS: clipboard_contents: dict = the data that will be represented
+    
+    EXAMPLE:
+        >>> show_clipboard_contents(clipboard_contents={'name': 'Mohamed'})
+        output: name
+                -----
+                Mohamed
+    
+    NOTE: it won't exactly look like this just pass in the dictionary to show, and that dictionary must only be 
+          the main data dictionary containing the json file's contents
+    """
     headers: list[str] = ['creation date', 'label', 'contents', 'priority']
     contents: list[str] = list(clipboard_contents.keys())
     rows: list[list] = []
--- src/helpers/sort.py
+++ src/helpers/sort.py
@@ -2,6 +2,18 @@ from datetime import datetime
 from . import clipboad_context
 
 def sort(highest: bool = False, newest: bool = False, sort_by_priority: bool = False) -> dict:
+    """
+    a function that sorts based on date or priority of the list when iterating through it to show
+    to the user
+    
+    PARAMS: highest: bool = it determines wether or not the priorites will be sorted from highest to lowest or vise versa (only works for priority sorting)
+    PARAMS: newest: bool = it determines wether or not the dates will be sorted from oldest to newest or vise versa (only works for date sorting)
+    PARAMS: sort_by_priority: bool = it determines wether or not the you will sort the by priority or by date
+    
+    EXAMPLE:
+        >>> sort(highest=True, newest=False, sort_by_priority=True)
+        return -> clipboard_contents_sorted
+    """
     clipboard_content: dict = clipboad_context.read_json()
         
     if highest is True and sort_by_priority is True:
